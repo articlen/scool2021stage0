bf698d4fa7128dd1d357b887bed0e0f02ec86039
articlen <igor9os4a@gmail.com>
2022-02-14 00:12:38 +0300

    feat: add code js

--- js30-2-1-random-jokes/index.js
+++ js30-2-1-random-jokes/index.js
@@ -1,1 +1,18 @@
-1
+// const rth = document.querySelector(".block-2one");
+// rth.classList.add("block-one");
+// console.log(rth.classList);
+const cort = document.querySelector(".block2one");
+cort.classList.add("blockone");
+
+// const container = document.querySelector(".container")
+// container.classList.add("container-styles");
+
+
+// console.log(container.classList);
+// const block = document.createElement("div")
+// block.classList.add("block")
+// container.append(block)
+
+// const rth = document.createElement("div");
+// rth.classList.add("block-one")
+// container.append(rth)
